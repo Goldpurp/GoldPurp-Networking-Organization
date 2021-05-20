3f8cd1c8ed6a2d87e2bacae03c5f698e8c6a1df9
GoldPurpp <lilswatch112@gmail.com>
2021-05-20 19:00:12 +0800

    commit GNO Age Check

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Goldpurp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("GoldPurp Networking Organization Employee intake");
+
+            Console.WriteLine("Enter Your Age");
+
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age < 0 || age > 100)
+
+            {
+                Console.WriteLine("Invalid age");
+            }
+            else if (age < 21)
+            {
+                Console.WriteLine("You're an underage");
+            }
+            else if (age > 45)
+            {
+                Console.WriteLine("You're too old for the task");
+            }
+            else
+            {
+                Console.WriteLine("Welcome TO G.N.O");
+            }
+
+        }
+    }
+}
